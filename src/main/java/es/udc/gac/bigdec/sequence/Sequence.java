720793d284dc6523cba2ff0699e5ce42ec7f7169
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-04 16:15:38 +0200

    Enhance setName, setBases, and setQuals methods

--- src/main/java/es/udc/gac/bigdec/sequence/Sequence.java
+++ src/main/java/es/udc/gac/bigdec/sequence/Sequence.java
@@ -42,21 +42,28 @@ public class Sequence {
 		this.quals = quals;
 	}
 
-	public void setName(byte[] name, int offset, int length) {
-		this.name = new byte[length];
-		System.arraycopy(name, offset, this.name, 0, length);
-	}
+    public void setName(byte[] name, int offset, int length) {
+        if (this.name == null || this.name.length < length) {
+        	this.name = new byte[Math.max(length, this.name == null ? 25 : this.name.length * 2)];
+        }
+        System.arraycopy(name, offset, this.name, 0, length);
+    }
 
-	public void setBases(byte[] bases, int offset, int length) {
-		this.bases = new byte[length];
-		System.arraycopy(bases, offset, this.bases, 0, length);
-	}
+    public void setBases(byte[] bases, int offset, int length) {
+        if (this.bases == null || this.bases.length < length) {
+        	this.bases = new byte[Math.max(length, this.bases == null ? 100 : this.bases.length * 2)];
+        }
+        this.length = (short) length;
+        System.arraycopy(bases, offset, this.bases, 0, length);
+    }
 
-	public void setQuals(byte[] quals, int offset, int length) {
-		this.quals = new byte[length];
-		System.arraycopy(quals, offset, this.quals, 0, length);
-	}
-
+    public void setQuals(byte[] quals, int offset, int length) {
+    	if (this.quals == null || this.quals.length < length) {
+        	this.quals = new byte[Math.max(length, this.quals == null ? 100 : this.quals.length * 2)];
+        }
+        System.arraycopy(quals, offset, this.quals, 0, length);
+    }
+	
 	public byte[] getName() {
 		return name;
 	}
